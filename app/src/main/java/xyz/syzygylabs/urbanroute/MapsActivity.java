5aa7ba17a5e6e24f4458ea91c4dd757ae0650d16
Charlie Wade <colson72098@gmail.com>
2018-02-20 15:15:38 -0500

    Setup Map

--- app/src/main/java/xyz/syzygylabs/urbanroute/MapsActivity.java
+++ app/src/main/java/xyz/syzygylabs/urbanroute/MapsActivity.java
@@ -28,10 +28,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.65428777,-83.60659127), 18.0f));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
